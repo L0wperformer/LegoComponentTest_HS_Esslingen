3c3669bf72e7b337cb3e4587408a06cd28950b3c
Rosenfeld, Pascal <rosenfeld.pascal1@gmail.com>
2022-05-12 13:48:22 +0200

    Added speed Slider for engines
    Also added some comments

--- ComponentTest.m
+++ ComponentTest.m
@@ -1,8 +1,6 @@
-% helloWorldButton = uibutton('push', 'ButtonPushedFcn',@(helloWorldButton,event) helloWorldButtonHandler());
-% 
-% function helloWorldButtonHandler()
-% disp('Hello World');
-% end
+%==================================================================
+%==========================User Interface==========================
+%==================================================================
 %I am used to the x-z coordinate System
 zMax = 400;
 %Width and height of a standart button
@@ -16,22 +14,35 @@ runEngineA = uibutton(fig,'push','Text','A','position',[225,zMax- 70,standartBut
 runEngineB = uibutton(fig,'push','Text','B','position',[225,zMax - 125,standartButton],'ButtonPushedFcn',@(runningEngineB,event) runEngine('B'));
 runEngineC = uibutton(fig,'push','Text','C','position',[225,zMax - 180,standartButton],'ButtonPushedFcn',@(runningEnginC,event) runEngine('C'));
 runEngineD = uibutton(fig,'push','Text','D','position',[225,zMax - 235,standartButton],'ButtonPushedFcn',@(runningEngineD,event) runEngine('D'));
-runTestProgram = uibutton(fig,'push','Text','Test Program','position',[20,zMax - 370,standartButton],'ButtonPushedFcn',@(runningEngineD,event) runEngine('testProgram'));
+runTestProgram = uibutton(fig,'push','Text','Test Program','position',[20,zMax - 390,standartButton],'ButtonPushedFcn',@(runningEngineD,event) runEngine('testProgram'));
+%Speed Slider
+speedSliderPanel = uipanel(fig,'Title','Motor speed %','position',[170,zMax-325,210,80]);
+speedSlider = uislider(fig,'Position',[183,zMax-280,180,3],'MajorTicks',[0,25,50,75,100]);
+speedSlider.Value = 50;
 %======Connections======
 uipanel(fig,'Title','Connections','position',[15, zMax- 195,110,210]);
-bluetoothConnect = uibutton(fig,'push','Text','via Bluetooth','position',[20, zMax- 70, standartButton],'ButtonPushedFcn',@(bluetoothConnect,event)btConnect());
-usbConnect =   uibutton(fig,'push','Text','via USB','position', [20,zMax - 130, standartButton],'ButtonPushedFcn',@(usbConnect,event)usbConnectFunc());       
-ethernetConnect = uibutton(fig,'push','Text','via Ethernet','position',[20,zMax - 190,standartButton],'ButtonPushedFcn',@(ethernetConnect,event)ethConnect());
-%======Sensors====== 
 %(Dont know why I did the buttons in relation to the
 %panel and not to the coordinate system)
 sensorPanel=uipanel(fig,'Title','Sensors','position',[400, zMax - 285, 150,300 ]);
+bluetoothConnect = uibutton(fig,'push','Text','via Bluetooth','position',[20, zMax- 70, standartButton],'ButtonPushedFcn',@(bluetoothConnect,event)btConnect());
+usbConnect =   uibutton(fig,'push','Text','via USB','position', [20,zMax - 130, standartButton],'ButtonPushedFcn',@(usbConnect,event)usbConnectFunc());       
+ethernetConnect = uibutton(fig,'push','Text','via Ethernet','position',[20,zMax - 190,standartButton],'ButtonPushedFcn',@(ethernetConnect,event)ethConnect());
+%======Sensors====== ;
 uipanel(sensorPanel,'Title','Proximity','position',[13,220,125,50]);
 sensor1 = uilabel(fig,'Text','Sensor 1','Position' ,[418,zMax - 75, standartButton],'FontSize',14);
 uipanel(sensorPanel,'Title','Ultrasonic','Position',[13,165,125,50]);
 sensor2 = uilabel(fig,'Text','Sensor 2','Position',[418,zMax - 130,standartButton],'FontSize',14);
 uipanel(sensorPanel,'Title','Gyrosensor','Position',[13,110,125,50]);
 sensor3 = uilabel(fig,'Text','Sensor 3','Position',[418,zMax - 185,standartButton],'FontSize',14);
+
+
+
+
+
+%==================================================================
+%==========================Functions===============================
+%==================================================================
+
 
 function runEngine(atOutput)
 %===DEBUG===
